fd46bd1743b55bc8b824642d565d581e84cde440
Bartint94 <96591176+Bartint94@users.noreply.github.com>
2022-01-04 19:31:45 +0100

    Commit
    Guns

---
+++ Assets/Scripts/GunBase.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunBase : MonoBehaviour
+{
+    protected float power;
+    protected float shot;
+    void Start()
+    {
+        
+    }
+    public void Shot(float shot)
+    {
+        this.shot = shot;
+    }
+   
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerInputs.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+[Serializable]
+public class ShotInput : UnityEvent<float> { }
+
+public class PlayerInputs : MonoBehaviour
+{
+    InputActions actions;
+
+
+    public ShotInput shotInput;
+
+
+    float shot;
+    
+    private void Awake()
+    {
+        actions = new InputActions();
+    }
+    private void OnEnable()
+    {
+        actions.Enable();
+        actions.Gameplay.Shoot.performed += OnShot;
+    }
+    public void OnShot(InputAction.CallbackContext cont)
+    {
+        shot = cont.ReadValue<float>();
+        shotInput.Invoke(shot);
+    }
+  
+
+
+}
