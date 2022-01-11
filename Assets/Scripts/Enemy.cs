b92349d9edcc28e4a5af39c4885b91e58272c250
Bartint94 <96591176+Bartint94@users.noreply.github.com>
2022-01-12 00:11:19 +0100

    update
    -fix enemy/boss script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,52 +5,30 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     Rigidbody rb;
-    [SerializeField] float hit;
-    [SerializeField] Transform Player;
-    [SerializeField] float Speed_f = 2;
+    
+    [SerializeField] Transform player;
+    [SerializeField] float speed = 2;
     Animator anim;
-    bool death_b = false;
-    public float angle;
+    
+    
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
     }
-    private void OnCollisionEnter(Collision collision)
-  
-    {
-       
-            if (collision.gameObject.CompareTag("bullet"))
-            {
-
-            
-            
-            Speed_f = 0;
-            death_b = true;
-                
-            }
-
-        
-    }
-
     
     void Update()
     {
-        angle = transform.localRotation.z;
-        if (angle>0.3f)
-        {
-            Speed_f = 0f;
-        }
+       
 
        
 
     }
     private void MoveEnemy()
     {
-        
-        rb.AddForce(Player.position * Speed_f, ForceMode.Acceleration);
-        anim.SetFloat("Speed_f", Speed_f);
-        anim.SetBool("Death_b", death_b);
+        transform.LookAt(player);
+        rb.AddRelativeForce(Vector3.forward * speed, ForceMode.Acceleration);
+        anim.SetFloat("Speed_f", speed);
     }
     private void FixedUpdate()
     {
